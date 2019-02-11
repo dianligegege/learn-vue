2f5743041f2dd3b3c15985c6a0963c14690679db
zhangli <zhangli@huobi.com>
2019-02-11 20:08:39 +0800

    提交

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <HelloWorld level="2">标题</HelloWorld>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "home",
+    components: {
+        HelloWorld
+    }
+};
+</script>
